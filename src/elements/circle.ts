67462fafe70bf702cc5d414e4c4be1d2114d1a6c
John van de Water <john.vd.water85@gmail.com>
2019-08-14 19:33:21 +0200

    Create path for circle

--- src/elements/circle.ts
+++ src/elements/circle.ts
@@ -1,4 +1,5 @@
 import withApplyStylesToCtx, { StyleObject } from './withApplyStylesToCtx'
+import { vec2 } from 'gl-matrix'
 
 interface props {
   x: number
@@ -9,14 +10,27 @@ interface props {
   style?: StyleObject
 }
 
-const drawCircle = (
-  ctx: CanvasRenderingContext2D,
-  { x, y, width, height, radius, style }: props
-) => {
-  ctx.beginPath()
-  ctx.arc(x, y, radius, 0, 2 * Math.PI)
-  ctx.fill()
-  if (style && style.lineWidth) ctx.stroke()
+const drawCircle = (ctx: CanvasRenderingContext2D, vnode) => {
+  const style = vnode.props.style
+  const path = vnode._path
+
+  ctx.stroke(path)
+
+  ctx.fill(path)
+  if (style && style.lineWidth) ctx.stroke(path)
+}
+
+export const createPath = vnode => {
+  const matrix = vnode._matrix
+  const { radius } = vnode.props
+  const position = vec2.fromValues(0, 0)
+
+  vec2.transformMat2d(position, position, matrix)
+
+  const p = new Path2D()
+  p.arc(position[0] + radius, position[1] + radius, radius, 0, 2 * Math.PI)
+
+  return p
 }
 
 export const draw = withApplyStylesToCtx(drawCircle)
