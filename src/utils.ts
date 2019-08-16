bf0584c029c457771468b6ba7974f03335206156
John van de Water <john.vd.water85@gmail.com>
2019-08-16 20:43:44 +0200

    start useEffect

--- src/utils.ts
+++ src/utils.ts
@@ -1,48 +1,10 @@
-const eventNames = {
-  onClick: 'click',
-  onMouseUp: 'mouseup',
-  onMouseDown: 'mousedown',
-  onMouseMove: 'mousemove',
-  onDoubleClick: 'doubleClick',
-  onWheel: 'wheel',
-  onTouchStart: 'touchstart',
-  onTouchEnd: 'touchend',
-  onTouchMove: 'touchmove',
-}
-
-const onEventNames = (() => {
-  let names = {}
-  for (let key in eventNames) {
-    names[eventNames[key]] = key
-  }
-  return names
-})()
-
-export function onEventToEventName(onEventName) {
-  const eventName = eventNames[onEventName]
-
-  if (eventName) return eventName
-
-  console.warn('no event name found for', onEventName)
-}
-
-export function eventNameToOnEventName(eventName) {
-  const onEventName = onEventNames[eventName]
-
-  if (onEventName) return onEventName
-
-  console.warn('no on event name found for', eventName)
-}
-
-export function overlap(point, rectangle) {
-  return (
-    point.x > rectangle.x &&
-    point.x < rectangle.x + rectangle.width &&
-    point.y > rectangle.y &&
-    point.y < rectangle.y + rectangle.height
-  )
-}
-
 export function deg2rad(deg) {
   return (deg * Math.PI) / 180
+}
+
+// from https://github.com/preactjs/preact/blob/8c984a276a1c8c52624769da64061d84849a5049/compat/src/index.js#L305
+export function shallowDiffers(a, b) {
+  for (let i in a) if (!(i in b)) return true
+  for (let i in b) if (a[i] !== b[i]) return true
+  return false
 }
