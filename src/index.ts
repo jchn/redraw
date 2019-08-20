920bf0c21415332fd3aeed66ade756ebc82e4e8a
John van de Water <john.vd.water85@gmail.com>
2019-08-20 19:10:32 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,4 @@ import { render } from './update'
 import { useState, useEffect } from './hooks'
 import { default as createElement } from './create-element'
 
-export default {
-  render,
-  useState,
-  useEffect,
-  createElement,
-}
+export { render, useState, useEffect, createElement }
